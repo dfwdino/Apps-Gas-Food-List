c8f9a0b53160b81a503b27b2fe4c3fcfaacbbb73
dfwdino <shane.newsom+github@gmail.com>
2014-02-28 16:34:01 -0600

    add/delete items stores.

--- SDNWebApps/Areas/GroceryList/Controllers/HomeController.cs
+++ SDNWebApps/Areas/GroceryList/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Data.Objects.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using SDNWebApps.Areas.GroceryList.Models.Home;
 
 namespace SDNWebApps.Areas.GroceryList.Controllers
 {
@@ -7,24 +12,111 @@ namespace SDNWebApps.Areas.GroceryList.Controllers
     {
         SDNAppsEntities sdnApps = new SDNAppsEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(bool showAll=false)
         {
-            var gitems = sdnApps.Items.Where(m => m.Have);
+            IQueryable<Item> gitems = sdnApps.Items;
+
+            if (!showAll)
+                gitems = gitems.Where(m => m.Have == showAll);
+
+            //a better way of doing this
+            var itemsVM = gitems.Select(m => new ViewItemsVM
+            {
+                ID = m.ID,
+                Name = m.Name,
+                Store = m.Store.StoreName,
+                Price = SqlFunctions.StringConvert((decimal) m.Price)
+
+            });
+
+            return View(itemsVM.ToList());
+        }
+
+        public ActionResult ListStores()
+        {
+            IQueryable<Store> gitems = sdnApps.Stores;
+
 
             return View(gitems);
         }
+
+        public ActionResult AddStore()
+        {
+            var newStore = new Store();
+
+            return View(newStore);
+        }
+
+        [HttpPost]
+        public ActionResult AddStore(string storeName)
+        {
+            var newStore = new Store();
+
+            newStore.StoreName = storeName;
+
+            sdnApps.Stores.Add(newStore);
+            sdnApps.SaveChanges();
+
+            return View(newStore);
+
+        }
+
+        public ActionResult AddItem()
+        {
+            var newItem = new Models.Home.ItemsViewModel();
+
+            newItem.ListToUse =
+                new SDNAppsEntities().Stores.OrderBy(m => m.StoreName).Select(m =>
+                    new SelectListItem { Value = SqlFunctions.StringConvert((double)m.ID).Trim(), Text = m.StoreName }).ToList();
+
+            return View(newItem);
+        }
+        
+        [HttpPost]
+        public JsonResult AddItem(string name, string price, int storeID)
+        {
+            if (price.IsNullOrWhiteSpace())
+                price = "0";
+
+            var newItem = new Item { Name = name, Price = Convert.ToDecimal(price), StoreID = storeID, Have = false };
+
+            sdnApps.Items.Add(newItem);
+            sdnApps.SaveChanges();
+            return Json(true);
+        }
+
         [HttpPost]
         public JsonResult DeleteItem(int itemID)
         {
             var ditem = sdnApps.Items.First(m => m.ID == itemID);
 
             sdnApps.Items.Remove(ditem);
-            //sdnApps.SaveChanges();
+            sdnApps.SaveChanges();
+
+            return Json("Record deleted successfully!");
+
+        }
+        public JsonResult DeleteStore(int itemID)
+        {
+            var ditem = sdnApps.Stores.First(m => m.ID == itemID);
+
+            sdnApps.Stores.Remove(ditem);
+            sdnApps.SaveChanges();
 
             return Json("Record deleted successfully!");
 
         }
 
+        [HttpPost]
+        public JsonResult GotItem(int itemID, bool haveItem = false)
+        {
+            var gotItem = sdnApps.Items.First(m => m.ID == itemID);
+
+            gotItem.Have = haveItem;
+            sdnApps.SaveChanges();
+
+            return Json("Record deleted successfully!");
+        }
 
     }
 }
--- SDNWebApps/Areas/GroceryList/Models/Home/ItemsViewModel.cs
+++ SDNWebApps/Areas/GroceryList/Models/Home/ItemsViewModel.cs
@@ -3,25 +3,52 @@ using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.WebPages.Html;
+using HtmlHelper = System.Web.Mvc.HtmlHelper;
+using System.Web.Routing;
 
 namespace SDNWebApps.Areas.GroceryList.Models.Home
 {
     public class ItemsViewModel
     {
-        public ItemsViewModel(ItemsViewModel itemsViewModel)
+        public ItemsViewModel()
         {
-            ItemName = itemsViewModel.ItemName;
-            Price = itemsViewModel.Price > 0.00 ? itemsViewModel.Price : 0.00;
-            //StoreID = itemsViewModel.StoreID;
-            StoreName = itemsViewModel.StoreName;
+            
         }
 
-        public string ItemName { get; set; }
-        public double Price { get; set; }
-        //public Guid StoreID { get; set; }
-        public string StoreName { get; set; }
+        public ItemsViewModel(ItemsViewModel itemsViewModel)
+        {
+        }
 
-        
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Store Store { get; set; }
+
+        public string SelectedItemId { get; set; }
+
+        public MvcHtmlString ListStores
+        {
+
+            get
+            {
+                SDNAppsEntities test = new SDNAppsEntities();
+                var ddl =test.Stores.Select(m => new {m.ID, m.StoreName})
+                    .ToList()
+                    .Select(m => new System.Web.Mvc.SelectListItem {Text = m.StoreName, Value = m.ID.ToString()})
+                    .ToList();
+
+                HtmlHelper htmlHelper = new HtmlHelper(new ViewContext(),null);
+
+                return htmlHelper.DropDownList("name", ddl, "select");
+            }
+        }
+
+        public List<System.Web.Mvc.SelectListItem> ListToUse { get; set; }
+       
+
+
 
     }
 }
