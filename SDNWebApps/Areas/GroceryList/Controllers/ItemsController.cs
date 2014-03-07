7026f5ab0d6e876f31d7b3a2b925aa1f4d52f179
dfwdino <shane.newsom+github@gmail.com>
2014-03-06 18:04:17 -0600

    Moved to more mvc style layout.

---
+++ SDNWebApps/Areas/GroceryList/Controllers/ItemsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+
+namespace SDNWebApps.Areas.GroceryList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        SDNAppsEntities sdnApps = new SDNAppsEntities();
+
+        [HttpPost]
+        public JsonResult GotItem(int itemID, bool haveItem = false)
+        {
+            var gotItem = sdnApps.Items.First(m => m.ID == itemID);
+
+            gotItem.Have = haveItem;
+            sdnApps.SaveChanges();
+
+            return Json("Record deleted successfully!");
+        }
+
+    public ActionResult AddItem()
+    {
+        var newItem = new Models.Home.ItemsViewModel();
+
+        newItem.ListToUse =
+            new SDNAppsEntities().Stores.OrderBy(m => m.StoreName).Select(m =>
+                new SelectListItem { Value = SqlFunctions.StringConvert((double)m.ID).Trim(), Text = m.StoreName }).ToList();
+
+        return View(newItem);
+    }
+
+    [HttpPost]
+    public JsonResult AddItem(string name, string price, int storeID)
+    {
+        if (price.IsNullOrWhiteSpace())
+            price = "0";
+
+        var newItem = sdnApps.Items.First(m => m.Name == name);
+
+        if (newItem == null)
+            newItem = new Item { Name = name, Price = Convert.ToDecimal(price), StoreID = storeID, Have = false };
+        else
+            newItem.Have = false;
+
+        sdnApps.Items.Add(newItem);
+        sdnApps.SaveChanges();
+        return Json(true);
+    }
+
+    [HttpPost]
+    public JsonResult DeleteItem(int itemID)
+    {
+        var ditem = sdnApps.Items.First(m => m.ID == itemID);
+
+        sdnApps.Items.Remove(ditem);
+        sdnApps.SaveChanges();
+
+        return Json("Record deleted successfully!");
+
+    }
+}
+}
---
+++ SDNWebApps/Scripts/items.js
@@ -0,0 +1,49 @@
+﻿
+    
+    $(document).ready(function () {
+        $('a.delete-link').click(OnDeleteClick);
+        $('a.got-link').click(OnGotClick);
+    });
+
+function OnDeleteClick(e)
+{
+    var rowdel = document.getElementById('FullItem{' + e.target.id + '}');
+    rowdel.parentNode.removeChild(rowdel);
+
+    $.post("/GroceryList/Items/DeleteItem", { itemID: e.target.id },
+          function (result) // success
+          {
+              alert('success');
+          },
+          function (result) // error
+          {
+              alert('error');
+          }
+         );
+    return false; // for the button
+}
+
+function OnGotClick(e) {
+    var rowdel = document.getElementById('FullItem{' + e.target.id + '}');
+    rowdel.parentNode.removeChild(rowdel);
+    var hasItem;
+
+    //need a find a better way
+    if (window.location.href.indexOf('showAll=True') < 0)
+        hasItem = true;
+    else {
+        hasItem = false;
+    }
+
+    $.post("/GroceryList/Items/GotItem", { itemID: e.target.id, haveItem: hasItem },
+          function (result) // success
+          {
+              alert('tea');
+          },
+          function (msg) // error
+          {
+              alert('Error');
+          }
+         );
+    return false; // for the button
+}
